58ac7d4ce2bb4bc48d78f0e459f47fae4d678443
Forbes-Sam <144731227+Forbes-Sam@users.noreply.github.com>
2023-09-16 12:23:24 -0600

    Finished the assinment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percent grade? ");
+        string sfPercentInput = Console.ReadLine();
+        float sfGrade = float.Parse(sfPercentInput);
+
+        if (sfGrade >= 90)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (sfGrade >= 80)
+        {
+            Console.WriteLine("You have an B");
+        } 
+        else if (sfGrade >= 70)
+        {
+            Console.WriteLine("You have an C");
+        } 
+        else if (sfGrade >= 60)
+        {
+            Console.WriteLine("You have an D");
+        } 
+        else if (sfGrade < 60)
+        {
+            Console.WriteLine("You have an F");
+        } 
+        else
+        {
+            Console.WriteLine("Not a valid input");
+        }
+
     }
 }
