b7036530fd9aa86bce043af20fa53670972dabee
Forbes-Sam <144731227+Forbes-Sam@users.noreply.github.com>
2023-09-21 13:41:06 -0600

    finished the core requierments for this assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
+// Sam Forbes
+// CSE 210
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int sfMagicNumber = randomGenerator.Next(1, 101);
+
+        Boolean incorrect = true;
+        while (incorrect)
+        {
+
+            Console.Write("What do you think the magic number is 1-100? ");
+            string sfGuessInput = Console.ReadLine();
+            int sfGuess = int.Parse(sfGuessInput);
+
+            if (sfGuess == sfMagicNumber)
+            {
+                incorrect = false;
+                Console.WriteLine($"You got it the magic number is {sfMagicNumber}");
+            }
+            else if (sfGuess >= sfMagicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (sfGuess <= sfMagicNumber)
+            {
+                Console.WriteLine("higher");
+            }
+
+        }
     }
 }
