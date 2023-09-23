d2c7f52658a937c512ed319291e04d1973c5add3
Forbes-Sam <144731227+Forbes-Sam@users.noreply.github.com>
2023-09-23 17:33:46 -0600

    project is not completly done I am still trying to figure out how to get the square of a number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,49 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+        static void sfDisplayWelcome ()
+        {
+            Console.WriteLine("Welcome to program");
+        }
+
+        static string sfPromptUserName ()
+        {
+            Console.Write("What is your name: ");
+            string sfName = Console.ReadLine();
+
+            return sfName;
+        }
+
+        static int sfPromptUserNumber ()
+        {
+            int sfNum = 0;
+            Console.Write("What is your Favorite number?: ");
+            string sfNumInput = Console.ReadLine();
+            sfNum = int.Parse(sfNumInput);
+
+            return sfNum;
+        }
+
+        static double sfSquareNumber (int sfNumber)
+        {
+            double sfSquareRoot = Math.Sqrt(sfNumber);
+
+            return sfSquareRoot;
+        }
+
+        static void sfDisplayResult (string sfName, double sfNumber)
+        {
+            Console.WriteLine($"{sfName}, the square of your number is {sfNumber}");
+        }
+
+        static void Main(string[] args)
+        {
+            sfDisplayWelcome();
+            string sfName = sfPromptUserName();
+            int sfNumber = sfPromptUserNumber();
+            double sfNumberSquared = sfSquareNumber(sfNumber);
+            sfDisplayResult(sfName, sfNumber);
+            
+        }
+    
 }
