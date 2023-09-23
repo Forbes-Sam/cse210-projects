a07591bf2bb82a95609a83b557a88de9c61696e2
Forbes-Sam <144731227+Forbes-Sam@users.noreply.github.com>
2023-09-23 16:56:17 -0600

    completed the assinment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
+using System.Transactions;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> sfNumbers = new List<int>();
+        int sfNumberInput = 1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (sfNumberInput != 0)
+        {
+            Console.Write("Enter a number: ");
+            string sfString = Console.ReadLine();
+            sfNumberInput = int.Parse(sfString);
+
+            sfNumbers.Add(sfNumberInput);
+        }
+
+        int sfSum = 0;
+        int sfLargest = 0;
+        foreach (int sfNum in sfNumbers)
+        {
+            sfSum = sfSum + sfNum;
+            
+            if (sfNum > sfLargest)
+            {
+                sfLargest = sfNum;
+            }
+        }
+
+        float sfAverage = sfSum / sfNumbers.Count;
+
+        Console.WriteLine($"The sum is: {sfSum}");
+        Console.WriteLine($"The average is: {sfAverage}");
+        Console.WriteLine($"The largest number is: {sfLargest}");
+
     }
 }
