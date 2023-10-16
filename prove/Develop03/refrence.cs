61b89973712e48ad43bf9741eb5ba83f9ba000ea
Forbes-Sam <144731227+Forbes-Sam@users.noreply.github.com>
2023-10-16 12:25:01 -0600

    added all of the classes to the repositrory and finished the refrence classe

---
+++ prove/Develop03/refrence.cs
@@ -0,0 +1,27 @@
+//Samuel Forbes
+using System;
+
+class reference
+{
+    private string sfGetReference;
+    public string sfGetText;
+    public list <string> sfResponse = new list <string>();
+
+    public reference()
+    {
+        sfGetReference = ""
+        sfGetText = ""
+    }
+    static list<string> sfGetPrompt()
+    {
+        Console.Write("What is the scripture reference? ");
+        sfGetReference = Console.ReadLine() ;
+
+        Console.write("What is the scripture? ");
+        sfGetText = console.ReadLine();
+
+        sfResponse.Add
+        
+        return sfResponse;
+    }
+}
